7f9347d15961c3d9233d2266a5de8ac32435e766
Douglas Medina <medina.douglas.dmv@gmail.com>
2026-02-13 20:10:02 -0300

    Refactor SQL query to retrieve clients who purchased in the last two draws but not in the current draw

--- marketing/query_compradores_que_nao_compraram_nos_ultimos_dois_sorteios.sql
+++ marketing/query_compradores_que_nao_compraram_nos_ultimos_dois_sorteios.sql
@@ -1,31 +1,42 @@
-SELECT
-    C.Nome,
-    C.Telefone,
-    C.Celular,
-    C.Email,
-    C2.NOME AS CIDADE,
-    u.Nome as UF
-FROM
-    cmsl_prod.Clientes C
-    LEFT JOIN cmsl_prod.Cidades C2 ON C2.ID = C.IDCIDADE
-    LEFT JOIN cmsl_prod.Ufs u on u.Id = C2.IdUF 
-WHERE
-    EXISTS (
-        SELECT 1
-        FROM cmsl_prod.Bilhetes B
-        WHERE B.IDCLIENTE = C.ID
-          AND B.IDCONCURSO = 73
-    )
-    AND EXISTS (
-        SELECT 1
-        FROM cmsl_prod.Bilhetes B
-        WHERE B.IDCLIENTE = C.ID
-          AND B.IDCONCURSO = 74
-    )
-    AND NOT EXISTS (
-        SELECT 1
-        FROM cmsl_prod.Bilhetes B
-        WHERE B.IDCLIENTE = C.ID
-          AND B.IDCONCURSO = 75
-    );
+-- QUERY PARA OBTER OS CLIENTES QUE REALIZARAM COMPRA NOS ULTIMOS DOIS CONCURSOS, MAS NÃO REALIZARAM NO CONCURSO ATUAL.
+-- Extrai automaticamente o Id do Concurso Atual validando a coluna "EhAtivo = 1" na tabela de Consursos
 
+WITH ConsursoAtual AS (
+	SELECT Id
+	FROM cmsl_prod.Concursos
+	WHERE EhAtivo = 1
+),
+
+UltimosConcursos AS (
+	SELECT Id
+	FROM cmsl_prod.Concursos
+	WHERE Id < (SELECT Id FROM ConsursoAtual)
+	ORDER BY Id DESC
+	LIMIT 2
+),
+
+ClientesUltimosDoisConcursos AS (
+	SELECT b.IdCliente
+	FROM cmsl_prod.Bilhetes b
+	WHERE b.IdConcurso in (SELECT Id FROM UltimosConcursos)
+	GROUP BY b.IdCliente
+	HAVING COUNT(DISTINCT b.IdConcurso) = 2
+)
+
+SELECT
+    clientes.Nome as NOME,
+    clientes.Telefone AS TELEFONE,
+    clientes.Celular AS CELULAR,
+    clientes.Email AS EMAIL,
+    cidades.NOME AS CIDADE,
+    ufs.Nome as UF
+FROM ClientesUltimosDoisConcursos as cudc
+JOIN cmsl_prod.Clientes as clientes on clientes.Id = cudc.IdCliente
+LEFT JOIN cmsl_prod.Cidades as cidades on cidades.Id = clientes.IdCidade
+LEFT JOIN cmsl_prod.Ufs as ufs on ufs.Id = cidades.IdUF
+WHERE NOT EXISTS (
+	SELECT 1
+	FROM cmsl_prod.Bilhetes as b
+	WHERE b.IdCliente = clientes.Id
+		AND b.IdConcurso = (SELECT Id FROM ConsursoAtual)
+)
